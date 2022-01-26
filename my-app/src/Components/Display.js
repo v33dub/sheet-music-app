b199133820ca3f072105f9bc4b5949b4f894b2e4
v33dub <46755823+v33dub@users.noreply.github.com>
2022-01-26 13:19:41 -0500

    Refactor display

--- my-app/src/Components/Display.js
+++ my-app/src/Components/Display.js
@@ -3,37 +3,20 @@ import trebleClef from '../images/trebleClef.png';
 
 export default class Display extends React.Component {
     render() {
+        let notes = ["E4", "F4", "G4", "A5", "B5", "C5", "D5", "E5", "F5"].reverse();
+        let keys = notes.map((note, i) => {
+            return (
+                <div className={i % 2 === 0 ? "line" : "space"} data-note={note}>
+                    {this.props.note === note ? <div className="note"></div> : ""}
+                </div>
+            );
+        });
+
         return (
             <div className="Display">
                 <div className="staff">
                     <img src={trebleClef} alt="Treble Clef" className="clef" />
-                    <div className="line" data-note="F5">
-                        {this.props.note === "F5" ? <div className="note"></div> : ""}
-                    </div>
-                    <div className="space" data-note="E5">
-                        {this.props.note === "E5" ? <div className="note"></div> : ""}
-                    </div>
-                    <div className="line" data-note="D5">
-                        {this.props.note === "D5" ? <div className="note"></div> : ""}
-                    </div>
-                    <div className="space" data-note="C5">
-                        {this.props.note === "C5" ? <div className="note"></div> : ""}
-                    </div>
-                    <div className="line" data-note="B5">
-                        {this.props.note === "B5" ? <div className="note"></div> : ""}
-                    </div>
-                    <div className="space" data-note="A5">
-                        {this.props.note === "A5" ? <div className="note"></div> : ""}
-                    </div>
-                    <div className="line" data-note="G4">
-                        {this.props.note === "G4" ? <div className="note"></div> : ""}
-                    </div>
-                    <div className="space" data-note="F4">
-                        {this.props.note === "F4" ? <div className="note"></div> : ""}
-                    </div>
-                    <div className="line" data-note="E4">
-                        {this.props.note === "E4" ? <div className="note"></div> : ""}
-                    </div>
+                    {keys}
                 </div>
             </div>
         );
