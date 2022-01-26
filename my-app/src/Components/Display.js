cbf9205819abf1ccffe9ecb7771b52e0b4b32ff0
v33dub <46755823+v33dub@users.noreply.github.com>
2022-01-26 10:22:04 -0500

    Add staff and note

--- my-app/src/Components/Display.js
+++ my-app/src/Components/Display.js
@@ -4,7 +4,35 @@ export default class Display extends React.Component {
     render() {
         return (
             <div className="Display">
-                <h1>{this.props.note}</h1>
+                <div className="staff">
+                    <div className="line" data-note="f">
+                        {this.props.note == "F" ? <div className="note"></div> : ""}
+                    </div>
+                    <div className="space" data-note="e">
+                        {this.props.note == "E" ? <div className="note"></div> : ""}
+                    </div>
+                    <div className="line" data-note="d">
+                        {this.props.note == "D" ? <div className="note"></div> : ""}
+                    </div>
+                    <div className="space" data-note="c">
+                        {this.props.note == "C" ? <div className="note"></div> : ""}
+                    </div>
+                    <div className="line" data-note="b">
+                        {this.props.note == "B" ? <div className="note"></div> : ""}
+                    </div>
+                    <div className="space" data-note="a">
+                        {this.props.note == "A" ? <div className="note"></div> : ""}
+                    </div>
+                    <div className="line" data-note="g">
+                        {this.props.note == "G" ? <div className="note"></div> : ""}
+                    </div>
+                    <div className="space" data-note="f">
+                        {this.props.note == "F" ? <div className="note"></div> : ""}
+                    </div>
+                    <div className="line" data-note="e">
+                        {this.props.note == "E" ? <div className="note"></div> : ""}
+                    </div>
+                </div>
             </div>
         );
     }
