35be5d37a7752a4d04e7b86cd317a09c3b9a5511
v33dub <46755823+v33dub@users.noreply.github.com>
2022-01-26 15:42:50 -0500

    Add sharp and flat notes to list of questions

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,15 +6,27 @@ import './index.css';
 
 ReactDOM.render(
   <App questions={10} notes={[
+      new Note("E", 4, false, true),
       new Note("E", 4, false, false),
       new Note("F", 4, false, false),
+      new Note("F", 4, true, false),
+      new Note("G", 4, false, true),
       new Note("G", 4, false, false),
+      new Note("G", 4, true, false),
+      new Note("A", 5, false, true),
       new Note("A", 5, false, false),
+      new Note("A", 5, true, false),
+      new Note("B", 5, false, true),
       new Note("B", 5, false, false),
       new Note("C", 5, false, false),
+      new Note("C", 5, true, false),
+      new Note("D", 5, false, true),
       new Note("D", 5, false, false),
+      new Note("D", 5, true, false),
+      new Note("E", 5, false, true),
       new Note("E", 5, false, false),
       new Note("F", 5, false, false),
+      new Note("F", 5, true, false),
     ]} 
   />,
   document.getElementById('root')
