901ab0097ea750b4f66080cea569a1302a2a05b8
v33dub <46755823+v33dub@users.noreply.github.com>
2022-01-31 15:10:08 -0500

    Get homepage working

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -1,32 +1,48 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './Home';
 import Game from './Game';
 import Note from '../Classes/Note';
 
-export default class App extends React.Component {
-  render() {
-    return <Game questions={10} notes={[
-        new Note("E", 4, false, true),
-        new Note("E", 4, false, false),
-        new Note("F", 4, false, false),
-        new Note("F", 4, true, false),
-        new Note("G", 4, false, true),
-        new Note("G", 4, false, false),
-        new Note("G", 4, true, false),
-        new Note("A", 5, false, true),
-        new Note("A", 5, false, false),
-        new Note("A", 5, true, false),
-        new Note("B", 5, false, true),
-        new Note("B", 5, false, false),
-        new Note("C", 5, false, false),
-        new Note("C", 5, true, false),
-        new Note("D", 5, false, true),
-        new Note("D", 5, false, false),
-        new Note("D", 5, true, false),
-        new Note("E", 5, false, true),
-        new Note("E", 5, false, false),
-        new Note("F", 5, false, false),
-        new Note("F", 5, true, false),
-      ]} 
-    />
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<PlayGame />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function PlayGame() {
+  return <Game questions={10} notes={[
+      new Note("E", 4, false, true),
+      new Note("E", 4, false, false),
+      new Note("F", 4, false, false),
+      new Note("F", 4, true, false),
+      new Note("G", 4, false, true),
+      new Note("G", 4, false, false),
+      new Note("G", 4, true, false),
+      new Note("A", 5, false, true),
+      new Note("A", 5, false, false),
+      new Note("A", 5, true, false),
+      new Note("B", 5, false, true),
+      new Note("B", 5, false, false),
+      new Note("C", 5, false, false),
+      new Note("C", 5, true, false),
+      new Note("D", 5, false, true),
+      new Note("D", 5, false, false),
+      new Note("D", 5, true, false),
+      new Note("E", 5, false, true),
+      new Note("E", 5, false, false),
+      new Note("F", 5, false, false),
+      new Note("F", 5, true, false),
+    ]} 
+  />
 }
